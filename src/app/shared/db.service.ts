4007c1f4dfe23b1228d6d5b687dba8419c467ccf
ajurasz <arkadiusz.jurasz@gmail.com>
2018-06-09 13:36:45 +0200

    Added some styles

--- src/app/shared/db.service.ts
+++ src/app/shared/db.service.ts
@@ -1,28 +1,33 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, flatMap } from 'rxjs/operators';
 import { Message, IMessage } from './message.model';
 
 @Injectable()
 export class DbService {
   constructor(private db: AngularFirestore) {}
   messages(): Observable<Message[]> {
-    return this.db
-      .collection('messages')
-      .valueChanges()
-      .pipe(
-        map(values => {
-          return values.map(value => {
-            const payload = value as IMessage;
-            console.log(payload);
-            return new Message(
-              payload.sender,
-              new Date(payload.timestamp),
-              payload.body
-            );
-          });
-        })
-      );
+    return (
+      this.db
+        // .collection('messages')
+        .collection('messages', ref => ref.orderBy('timestamp', 'desc'))
+        .stateChanges(['added'])
+        .pipe(
+          map(values => {
+            return values.map(value => {
+              const id = value.payload.doc.id;
+              const payload = value.payload.doc.data() as IMessage;
+
+              return new Message(
+                id,
+                payload.sender,
+                new Date(payload.timestamp),
+                payload.body
+              );
+            });
+          })
+        )
+    );
   }
 }
--- src/app/shared/message.model.ts
+++ src/app/shared/message.model.ts
@@ -1,12 +1,18 @@
 export class Message {
   constructor(
+    public id: string,
     public sender: string,
     public timestamp: Date,
     public body: string
   ) {}
+
+  static sample(): Message {
+    return new Message('id', 'name', new Date(), 'message');
+  }
 }
 
 export interface IMessage {
+  id: string;
   sender: string;
   timestamp: number;
   body: string;
